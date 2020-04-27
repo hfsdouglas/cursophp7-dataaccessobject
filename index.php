63ecab3e091fe47cce3583b45306f32a7c8b4d7f
Douglas Faria <hfsdouglas@gmail.com>
2020-04-27 13:49:18 -0300

    Criação da classe usuário

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
     require_once ("config.php");
     $sql = new Sql;
-    $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-    echo json_encode($usuarios);
+    
+    /*$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+    echo json_encode($usuarios);*/
+
+    $junior = new Usuarios();
+    $junior->loadById(3);
+
+    echo $junior;
